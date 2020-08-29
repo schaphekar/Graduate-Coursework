14489e2b8701c5159a3c171b4fc31d58a76f2c3d
Siddharth Chaphekar <61927942+schaphekar@users.noreply.github.com>
2020-08-28 19:27:12 -0700

    Update firstUnique.py

--- CSC 421 - Applied Algorithms & Structures/firstUnique.py
+++ CSC 421 - Applied Algorithms & Structures/firstUnique.py
@@ -2,8 +2,10 @@
 
 def first_unique(s):
 	
+	# Dictionary that tracks letter occurrences
 	lettercount = {}
 
+	# Iterate through string to add letters to dictionary
 	for letter in s:
 		if letter not in lettercount:
 			lettercount[letter] = 1

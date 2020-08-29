67b6c594ed4f8ff2cff7f109956496ccc9d0b580
Siddharth Chaphekar <61927942+schaphekar@users.noreply.github.com>
2020-08-28 19:25:53 -0700

    Add files via upload

---
+++ CSC 421 - Applied Algorithms & Structures/firstUnique.py
@@ -0,0 +1,23 @@
+### This function takes a string as input and returns the first unique letter or character.
+
+def first_unique(s):
+	
+	lettercount = {}
+
+	for letter in s:
+		if letter not in lettercount:
+			lettercount[letter] = 1
+		else:
+			lettercount[letter] += 1
+
+	for letter in lettercount:
+		if lettercount[letter] == 1:
+			return letter
+
+	return None
+
+# Sample output, prints 'c'
+print(first_unique('ababca'))
+ 
+# Sample output, prints '$'
+print(first_unique('aaaaa$1#szs'))
